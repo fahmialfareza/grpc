e7c9c453518ba1e700c53593be6d900f95302bb7
Fahmi Alfareza <fahmialfareza@icloud.com>
2023-06-07 12:22:53 +0700

    feat: fix the proto name

--- proto/generate_proto.sh
+++ proto/generate_proto.sh
@@ -2,11 +2,24 @@
 
 PROTO_DIR="."
 
+# Array to track filenames
+declare -a filenames
+
 # Find all .proto files in the proto directory and generate the corresponding files
 find "$PROTO_DIR" -name "*.proto" -print0 | while IFS= read -r -d '' file; do
     # Extract the directory and filename without extension
     dir=$(dirname "$file")
     filename=$(basename "$file" .proto)
+
+    # Check if the filename already exists in the array
+    if [[ " ${filenames[*]} " == *" $filename "* ]]; then
+        echo "Error: Duplicate filename detected: $filename"
+        echo "File: $file"
+        exit 1
+    fi
+
+    # Store the filename in the array
+    filenames+=("$filename")
 
     # Generate the Go protobuf files in the same directory as the source file
     protoc --proto_path="$PROTO_DIR"  --go_out="$dir/.." --go-grpc_out="$dir/.." "$file"
